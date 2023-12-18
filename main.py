d113b4753a1d3a8baebf521874ae83c19622fdbc
Lancet_ <86073946+Lancet-crow@users.noreply.github.com>
2023-12-18 19:55:26 +0400

    Начало подготовки
    Добавлены файлы основного цикла игры, загрузки ресурсов и стартового экрана + проект для фона стартового экрана в Adobe Illustrator.

---
+++ main.py
@@ -0,0 +1,19 @@
+import pygame
+
+from Tid.start_screen import start_screen
+
+pygame.init()
+size = width, height = 1920, 1080
+screen = pygame.display.set_mode(size)
+FPS = 60
+clock = pygame.time.Clock()
+running = True
+
+start_screen(screen, clock, FPS)
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    screen.fill((0, 0, 0))
+    pygame.display.flip()
+pygame.quit()
